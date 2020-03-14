4be33a2ecf91c53b9be41c65b2d2447741e753c2
andrayantelo <andrayantelo@gmail.com>
2020-03-13 17:24:45 -0700

    Updates

---
+++ precache-manifest.197d1acc7ef1eb782d8be58cbe5be25f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adf0ea58bc57ae32f19dda26210d6a9",
+    "url": "/react-markdown-previewer/index.html"
+  },
+  {
+    "revision": "5fc085d0f41a49f87ac6",
+    "url": "/react-markdown-previewer/static/css/main.d8d1f265.chunk.css"
+  },
+  {
+    "revision": "f7ddf649c976edcb34ad",
+    "url": "/react-markdown-previewer/static/js/2.910b6a37.chunk.js"
+  },
+  {
+    "revision": "5fc085d0f41a49f87ac6",
+    "url": "/react-markdown-previewer/static/js/main.531ff767.chunk.js"
+  },
+  {
+    "revision": "eb9a90298d8c9c1b4288",
+    "url": "/react-markdown-previewer/static/js/runtime-main.41284f4a.js"
+  }
+]);
